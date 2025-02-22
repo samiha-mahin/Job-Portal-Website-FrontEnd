e0e8fae9826ed6cb2c89217c0fe1f8a6436af2ec
samiha-mahin <samihamahin36@gmail.com>
2025-02-23 00:20:01 +0600

    job-portal

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -35,7 +35,7 @@ const Jobs = () => {
                     {
                        filterJobs.length <= 0 ? <span>Job not found!</span> : (
                         <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
-                            <div className='grid grid-cols-3 gap-4'>
+                            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
                                 {
                                     filterJobs.map((job) =>(
                                         <div key={job?._id}>
