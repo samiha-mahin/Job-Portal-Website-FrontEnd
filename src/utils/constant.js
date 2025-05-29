1b0c9c7587ac3ab8ac14a797da697d1af3605388
samiha-mahin <samihamahin36@gmail.com>
2025-05-30 01:36:22 +0600

    job portal

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,9 +1,4 @@
-  // export const USER_API = "http://localhost:8000/api/v1/user";
-  // export const JOB_API = "http://localhost:8000/api/v1/job";
-  // export const APPLICATION_API = "http://localhost:8000/api/v1/app";
-  // export const COMPANY_API = "http://localhost:8000/api/v1/company";
-
- export const USER_API = process.env.REACT_APP_USER_API;
- export const JOB_API = process.env.REACT_APP_JOB_API;
- export const APPLICATION_API = process.env.REACT_APP_APPLICATION_API;
- export const COMPANY_API = process.env.REACT_APP_COMPANY_API;
+export const USER_API = import.meta.env.VITE_USER_API;
+export const JOB_API = import.meta.env.VITE_JOB_API;
+export const APPLICATION_API = import.meta.env.VITE_APPLICATION_API;
+export const COMPANY_API = import.meta.env.VITE_COMPANY_API;
