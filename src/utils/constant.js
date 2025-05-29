0ada933235d771879479a9689c8369c57fdcb89b
samiha-mahin <samihamahin36@gmail.com>
2025-05-30 00:48:42 +0600

    job portal

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,7 @@
   // export const APPLICATION_API = "http://localhost:8000/api/v1/app";
   // export const COMPANY_API = "http://localhost:8000/api/v1/company";
 
- export const USER_API = "https://job-portal-website-back-end.vercel.app/api/v1/user";
- export const JOB_API = "https://job-portal-website-back-end.vercel.app/api/v1/job";
- export const APPLICATION_API = "https://job-portal-website-back-end.vercel.app/api/v1/app";
- export const COMPANY_API = "https://job-portal-website-back-end.vercel.app/api/v1/company";
+ export const USER_API = process.env.REACT_APP_USER_API;
+ export const JOB_API = process.env.REACT_APP_JOB_API;
+ export const APPLICATION_API = process.env.REACT_APP_APPLICATION_API;
+ export const COMPANY_API = process.env.REACT_APP_COMPANY_API;
